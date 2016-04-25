86c88addb3d94c182d3e32edab12cbf2dc5dc4f6
chanlengr <chanlengr@sina.com>
2016-04-25 09:34:17 +0800

    t

--- src/Main/stockAIMain.java
+++ src/Main/stockAIMain.java
@@ -1,8 +1,11 @@
 package Main;
 
+import stockAI.stockDeal;
+
 public class stockAIMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub00000
+		// TODO Auto-generated method
+		stockAI.stockDeal.stockBuyCalc(100000,0.75,50.31);
 	}
 }
--- src/stockAI/stockDeal.java
+++ src/stockAI/stockDeal.java
@@ -1,24 +1,40 @@
 package stockAI;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class stockDeal {
-	public static String stockBuyCalc(double stockPrice,double availableMoney,double buyPercent)
+	public static ArrayList<String> stockBuyCalc(double availableMoney,double buyPercent,double stockPrice)
 	{
-		double availableBuyMoney;   //可以购买的金额
-		double buyAmount;		//买入多少手
-		double lastMoney;
-		double costMount;
-		DecimalFormat df2 = new DecimalFormat("0.00");
+		double availableBuyMoney;   //可购买的金额
+		int buyAmount;		//买入多少手
+		double costMount;		//花费
+		double lastMoney;		//剩余金额
 		
-		availableBuyMoney=availableMoney*buyPercent;
+		ArrayList<String> buyList = new ArrayList<String>();
 		
-		buyAmount=availableBuyMoney/(stockPrice*100);  //单位手
-		costMount=(int)buyAmount*stockPrice*100;
-		lastMoney=availableMoney-(int)buyAmount*stockPrice*100;
-		System.out.println("可以购买的金额:" + availableBuyMoney + "\n买入手数: " + (int)buyAmount+ "\n花费: " + costMount + "\n剩余金额:" +df2.format(lastMoney));
+		DecimalFormat df2 = new DecimalFormat("0.0");
 		
-		return null;
+		availableBuyMoney=availableMoney*buyPercent;   //可购买的金额
+		
+		buyAmount=(int)(availableBuyMoney/(stockPrice*100));  //单位手
+		
+		costMount=buyAmount*stockPrice*100; //花费金额
+		
+		lastMoney=availableMoney-(int)buyAmount*stockPrice*100;  //剩余金额
+		
+		buyList.add(df2.format(availableMoney));	//当前总金额
+		buyList.add(df2.format(availableBuyMoney));//可购买的金额
+		buyList.add(df2.format(buyAmount));		//单位手
+		buyList.add(df2.format(costMount));		//花费金额
+		buyList.add(df2.format(lastMoney));		//剩余金额
+		
+		System.out.println(buyList);
+		
+		System.out.println("当前总金额:" + buyList.get(0) + "\n可以购买的金额:" + buyList.get(1) 
+		+ "\n买入手数: " + buyList.get(2)+ "\n花费: " + buyList.get(3) + "\n剩余金额:" +buyList.get(4));
+		
+		return buyList;
 		
 	}
 	public String stockSellCalc()
